f24820481a1100cabbe97267f6bae5b3d9c47e18
SHYAM HARODE <100039836+ShyamHarode@users.noreply.github.com>
2022-12-08 18:13:12 +0530

    UI improvement

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -24,19 +24,18 @@ const Item = ({ item, index, add, remove, cart }) => {
     } else {
       setFlag(false);
     }
-    // eslint-disable-next-line
   }, [cart, remove]);
 
   return (
-    <div className="w-64  h-96 bg-white flex flex-col justify-between rounded-md border border-gray overflow-hidden shadow-lg">
+    <div className="w-64 p-2 h-96 bg-white flex flex-col justify-between rounded-md border border-gray overflow-hidden shadow-md hover:shadow-white">
       <div>
+        <div className="font-bold mb-2 ml-2">{item.name}</div>
         <img
-          className="w-full h-48 hover:scale-110 hover:duration-200"
+          className="w-60 h-48 hover:scale-110 hover:duration-200 rounded-lg"
           src={`assets/${item.image}`}
           alt="item"
         />
         <div className="px-6 py-2 ">
-          <div className="font-bold mb-2">{item.name}</div>
           <p className="text-gray-700 text-base">Price: ₹ {item.price}</p>
           {flag && (
             <div>
@@ -46,17 +45,17 @@ const Item = ({ item, index, add, remove, cart }) => {
           )}
         </div>
       </div>
-      <div className="px-6 pb-1 ">
+      <div className="px-5 pb-1 ">
         <button
           onClick={() => add(index)}
-          className="bg-indigo-800  w-11 px-8 py-1 btn m-2 items-center flex-col rounded text-xl"
+          className="bg-indigo-800  w-11 px-8 py-1 btn m-2 items-center flex-col rounded text-xl hover:bg-purple-900 "
         >
           +
         </button>
         <button
           onClick={() => remove(index)}
           className={`${
-            flag ? "bg-rose-600" : "bg-gray-300"
+            flag ? "bg-rose-600 hover:bg-rose-900" : "bg-gray-300"
           }  px-8 py-1  btn m-2 rounded items-center flex-col text-xl`}
         >
           -
